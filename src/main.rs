7418a1f2d749711bde400c0eafabbce25e68840c
yuniruyuni <yuniruyuni@gmail.com>
2021-06-08 05:48:00 +0900

    create push method

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
+use std::fmt;
+
+struct LinkedList<T> {
+    root: Option<Box<Node<T>>>,
+    size: usize,
+}
+
+struct Node<T> {
+    next: Option<Box<Node<T>>>,
+    value: T,
+}
+
+
+impl<T> LinkedList<T> where T: fmt::Display {
+    fn new() -> Self {
+        return LinkedList {
+            root: None,
+            size: 0,
+        }
+    }
+
+    fn push(&mut self, v: T) {
+        let newroot = Some(Box::new(Node {
+            next: self.root.take(),
+            value: v,
+        }));
+        self.root = newroot;
+        self.size += 1;
+    }
+
+    fn print_all(&self) {
+        let mut current = self.root.as_ref();
+
+        while let Some(node) = current {
+            println!("{}", node.value);
+            current = node.next.as_ref();
+        }
+    }
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let mut l1 = LinkedList::new();
+    l1.push("abc");
+    l1.push("def");
+    l1.print_all();
 }
